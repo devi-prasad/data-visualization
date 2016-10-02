7e3661d85587bcd346bbbedbce57f4bfc0cb827d
dprasad <dprasadm@gmail.com>
2016-10-02 13:43:04 +0530

    The first version: converting series of headers to a row in a big table. Each row is for now represented by a Python dictionary. Need to write this to a simple text table so we can import it as a DataFrame in Pandas.

--- examples/gmail-analyzer/mktbl.py
+++ examples/gmail-analyzer/mktbl.py
@@ -5,11 +5,11 @@ import contextlib
 import mmap
 
 keywords = ['from', 'to', 'date', 'content-type', 'subject', 'cc', 'bcc']
-fence = '+'*80 + '\n'
+multiline_keywords = ['to', 'cc', 'bcc', 'subject']
+punctuations = [' ', '\t', '\n', ',', ';', '<', '>', '/', '\"', '\'', '&', '#', '=']
+fence = '++##++\n'
 
-def new_row():
-  return {'from': '', 'to': '', 'date': '', 'day': '', 'cc': '', 'bcc': '', 'subject': ''}
-
+###
 def grep_one_email_id(s, cur=0):
   assert(s is not None and cur >= 0)
   ##
@@ -22,13 +22,14 @@ def grep_one_email_id(s, cur=0):
       eid = (s[l+1:r].split())[0]
   ##
   if eid is None: # may be the email id is not surrounded by angle braces < ... >
+    l = r = -1
     mid = s.find('@', cur) # abcd@wxyz
     if (mid >= 0):
       l = r = mid
       slen = len(s)
-      while (l >= 0 and s[l] not in [' ', '\t', '\n', ',', ';', '<', '\"', '\'']):
+      while (l >= 0 and s[l] not in punctuations):
         l = l - 1
-      while (r < slen and s[r] not in [' ', '\t', '\n', ',', ';', '>', '\"', '\'']):
+      while (r < slen and s[r] not in punctuations):
         r = r + 1
       eid = (s[l+1: r].split())[0]
   ##
@@ -36,7 +37,7 @@ def grep_one_email_id(s, cur=0):
   ##
   return (r, eid)
 
-
+###
 def grep_email_ids(s):
   i = 0
   eid = None
@@ -49,78 +50,92 @@ def grep_email_ids(s):
   ##
   return eids
 
-##
+###
 ## 'run' looks like this: 'Fri, 8 Nov 2013 22:00:14 +0530'
 def parse_line_content(row, kw, run):
   #
   if kw == 'from':
     eids = grep_email_ids(run)
-    if (len(eids) == 0): eids = ['']
-    print('from: ', eids[0])
+    if (len(eids) == 1):
+      row['from'] = eids[0]
+    else: row['from'] = ' '
   if kw == 'to':
     to = grep_email_ids(run)
-    print('to:', to)
+    if (len(to) >= 1):
+      row['to'] = to
+    else: row['to'] = ' '
   if kw == 'cc':
     cc = grep_email_ids(run)
-    print('cc:', cc)
+    if (len(cc) == 0):
+      row['cc'] = cc
+    else: row['cc'] = ' '
   if kw == 'bcc':
     bcc = grep_email_ids(run)
-    print('bcc:', bcc)
+    if (len(bcc) == 0):
+      row['bcc'] = bcc
+    else: row['bcc'] = ' '
   if kw == 'date':
   	daydate = run.split()
   	row['day'] = daydate[0][0:3]
   	row['date'] = daydate[1:4]
-  	print('day and date: ', row['day'], row['date'])
   if kw == 'content-type':
     row['content-type'] = run.split()[0][:-1]
-    print('content-type: ', row['content-type'])
   if kw == 'subject':
     row['subject'] = run.strip('\'\" ')
-    print('subject: ', row['subject'])
   ##
   return row
 
+###
+def table_write(row):
+  if 'from' in row:
+    print(row)
 
+###
 def make_table(f):
-  pattern = re.compile("^(?P<keyword>[-\w]*):(?P<run>.*$)", 
-                       re.IGNORECASE | re.DOTALL | re.MULTILINE)
-  row = None
+  pattern = re.compile("^(?P<keyword>[-\w]*):(?P<run>.*$)",
+                       re.IGNORECASE | re.DOTALL)
+  row = {}
+  nextline = None
   ##
-  with f:
-    for l in f:
-      if (l == fence):
-        row = new_row()
-      else:
-        m = pattern.match(l)
-        if m is not None:
-          kw = m.group('keyword').lower()
-          if kw in keywords:
-            row = parse_line_content(row, kw, m.group('run').lower())
-            #print((kw, m.group('run').lower()))
-            #print(row)
+  while True:
+    if nextline is None:
+      l = f.readline()
+    else:
+      l = nextline
+      nextline = None
+    ##
+    if l == '' or l is None:
+      table_write(row);
+      break
+    if (l == fence):
+      table_write(row);
+      row = {}
+    else:
+      m = pattern.match(l)
+      if m is not None:
+        kw = m.group('keyword').lower()
+        run = m.group('run').lower()
+        if kw in keywords:
+          if kw in multiline_keywords:
+            for nl in f:
+              m = pattern.match(nl)
+              if m is None:
+                nlp = nl.find('=')
+                if (nlp > -1): nl = nl[nlp:]
+                run = run + nl.lower()
+                #print(run)
+              else:   ## we consumed an additional line; make a note!
+                nextline = nl
+                break
+          ##
+          ## we got hold of the run text - even the multi-line ones.
+          row = parse_line_content(row, kw, run)
 
-def make_table_rough_bad(f):
-  pattern = re.compile(b"^(?P<keyword>[-\w]*):(?P<run>.*$)",
-                       re.IGNORECASE | re.DOTALL | re.MULTILINE)
-  row = None
-  ##
-  with contextlib.closing(mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)) as mf:
-    #while True:
-      m = pattern.findall(mf)
-      print(m[0][0])
-      #if m is not None:
-      #  kw = m.group('keyword').lower()
-      #  print(kw, m.group('run').lower())
-      #  if kw in keywords:
-          #row = parse_line_content(row, kw, m.group('run').lower())
-      #    print((kw, m.group('run').lower()))
-          #print(row)
-
-
+###
 def main(mail_headers_path):
   f = io.open(mail_headers_path, 'rt', encoding='utf-8')
   make_table(f)
   f.close()
 
-#main('gms.py')
+###
 main('gmheaders.txt')
